abb75969f92ff343ff85c660a09fdfb4ad280f75
Nick Kinnaird <nickkinn@bu.edu>
2021-02-07 16:39:17 -0600

    Added ROC and precision recall curve plots.

--- code/modelEvalUtils.py
+++ code/modelEvalUtils.py
@@ -21,3 +21,25 @@ def printMetricsAndConfMat(y_train, y_pred, modelAbrev):
     plt.ylabel('actual')
     plt.title(modelAbrev + " Conf Mat");
     plt.show();
+    
+    
+    
+def makeMetricPlots(pipeline, inputX, inputY, model_name):
+
+    # plot ROC curve
+    roc = metrics.plot_roc_curve(pipeline, inputX, inputY, name=model_name)
+
+    # plot precision and recall curves against threshold
+    precision_curve, recall_curve, threshold_curve = metrics.precision_recall_curve(inputY, pipeline.predict_proba(inputX)[:,1] )
+
+    plt.figure(dpi=80)
+    plt.plot(threshold_curve, precision_curve[1:],label='precision')
+    plt.plot(threshold_curve, recall_curve[1:], label='recall')
+    plt.legend(loc='lower left')
+    plt.xlabel('Threshold (above this probability, label as conflicting)');
+    plt.title('Precision and Recall Curves');
+    
+    
+    # plot precision curve vs recall curve
+    prec_vs_rec = metrics.plot_precision_recall_curve(pipeline, inputX, inputY, name=model_name)
+
